6ac7803848e25f4700f058e07d4ff0d151c44ac2
LinkCastling <1323113442@qq.com>
2021-09-05 22:49:05 +0800

    PlayerController测试代码

--- Silksong/Assets/Scripts/MovementScript.cs
+++ Silksong/Assets/Scripts/MovementScript.cs
@@ -7,30 +7,30 @@ public static class MovementScript
     //Put your static method here for reuse
 
     /// <summary>
-    /// �ƶ��������ڴ����position�Ļ����ϸı�λ��
+    /// A function that moves a specified distance
     /// </summary>
-    /// <param name="movement">�ƶ���Vector2</param>
-    /// <param name="nextMovement">���ı��Vector2</param>
+    /// <param name="movement">Move distance of Vector2</param>
+    /// <param name="nextMovement">Vector2 being moved</param>
     public static void Move(Vector2 movement, ref Vector2 nextMovement)
     {
         nextMovement += movement;
     }
 
     /// <summary>
-    /// ���ͺ���������ֱ�Ӵ��͵�ָ����position
+    /// Teleport to the specified location
     /// </summary>
-    /// <param name="position">Ҫ���͵�λ��</param>
-    /// <param name="rigidbody2D">�����͵�����ĸ���</param>
+    /// <param name="position">The location to teleport</param>
+    /// <param name="rigidbody2D">The rigidbody of the object being teleported</param>
     public static void Teleport(Vector2 position,  Rigidbody2D rigidbody2D)
     { 
         rigidbody2D.MovePosition(position);
     }
     /// <summary>
-    /// ��̺���
+    /// The sprint function
     /// </summary>
-    /// <param name="speed">��̵��ٶȣ���̵ľ����ɳ���ٶȾ���</param>
-    /// <param name="direction">��̵ķ���</param>
-    /// <param name="rigidbody2D">Ҫ���г������ĸ���</param>
+    /// <param name="speed">The sprint speed</param>
+    /// <param name="direction">The sprint direction</param>
+    /// <param name="rigidbody2D">To sprint the rigidbody of the object</param>
     public static void Sprint(float speed, Vector2 direction, Rigidbody2D rigidbody2D)
     {
         rigidbody2D.AddForce(new Vector2(direction.x  * speed * Time.deltaTime , direction.y));
--- Silksong/Assets/Scripts/PlayerController.cs
+++ Silksong/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,21 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
-    //冲刺
+    //sprint
     private bool isRun;
     public float runRate;
     float currSpeed;
-
-
-
+    //move
+    public Vector2 distance;
+    public Vector2 nextDistance;
+    private Rigidbody2D rd;
+    //Teleport
+    public GameObject telePosition;
     // Start is called before the first frame update
     void Start()
     {
         currSpeed = speed;
-
+        rd = GetComponent<Rigidbody2D>();
     }
 
 
@@ -25,11 +28,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CheckIsRun();
+        
     }
 
     private void FixedUpdate()
     {
-
+        //rd.MovePosition(rd.position + Vector2.left * speed * Time.fixedDeltaTime);
+        MoveMent();
     }
     /// <summary>
     /// //冲刺判断
@@ -48,5 +53,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void MoveMent()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("左");
+            nextDistance = transform.position;
+            Vector2 direction = Vector2.left * speed * Time.fixedDeltaTime;
+            MovementScript.Move(direction, ref nextDistance);
+            rd.position = nextDistance;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("右");
+            nextDistance = transform.position;
+            Vector2 direction = Vector2.right * speed * Time.fixedDeltaTime;
+            MovementScript.Move(direction, ref nextDistance);
+            rd.position = nextDistance;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("冲刺");
+            MovementScript.Sprint(100f, transform.position, rd);
+        }
+
+        if(Input.GetKey(KeyCode.X))
+        {
+            Debug.Log("Teleport");
+            MovementScript.Teleport(telePosition.transform.position, rd);
+        }
+    }
 
 }
