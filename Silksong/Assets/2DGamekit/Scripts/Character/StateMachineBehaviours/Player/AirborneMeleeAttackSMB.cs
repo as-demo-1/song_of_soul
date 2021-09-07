efea56306e9a4f865f56654c3fa44284c58668e7
BN <sijinwu114@gmail.com>
2021-09-08 01:34:04 +0800

    Set the HollowKnight anims

---
+++ Silksong/Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/AirborneMeleeAttackSMB.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit2D
+{
+    public class AirborneMeleeAttackSMB : SceneLinkedSMB<PlayerCharacter>
+    {
+        public override void OnSLStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.ForceNotHoldingGun();
+        }
+
+        public override void OnSLStatePostEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.EnableMeleeAttack();
+            if (m_MonoBehaviour.dashWhileAirborne)
+                m_MonoBehaviour.SetHorizontalMovement(m_MonoBehaviour.meleeAttackDashSpeed * m_MonoBehaviour.GetFacing());
+        }
+
+        public override void OnSLStateNoTransitionUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.UpdateJump();
+            m_MonoBehaviour.AirborneHorizontalMovement();
+            m_MonoBehaviour.AirborneVerticalMovement();
+            m_MonoBehaviour.CheckForGrounded();
+        }
+
+        public override void OnSLStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.DisableMeleeAttack();
+        }
+    }
+}
---
+++ Silksong/Assets/Scripts/AnimationStateParamsSetter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateParamsSetter
+{
+    public void ParamsUpdate()
+    {
+        SetAnimatorParamsTofalse();
+    }
+
+    void SetAnimatorParamsTofalse()
+    {
+
+    }
+}
