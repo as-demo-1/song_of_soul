dfae1085e87fb91deb65027bc8cef4fc0db84d34
Isaac <isaackwan105@gmail.com>
2021-10-16 03:43:16 -0500

    Add Inventory System and Update Save

---
+++ Silksong/Assets/Scripts/Inventory/ScriptableObjects/ItemSO.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class ItemSO : SerializableScriptableObject
+{
+    [Tooltip("The name of the item")]
+    [SerializeField] private string _name = default;
+
+    [Tooltip("A preview image for the item")]
+    [SerializeField]
+    private Sprite _previewImage = default;
+
+    [Tooltip("A description of the item")]
+    [SerializeField]
+    private string _description = default;
+
+    [Tooltip("A description of the item")]
+    [SerializeField]
+    private int _healthResorationValue = default;
+
+    [Tooltip("The type of item")]
+    [SerializeField]
+    private ItemTypeSO _itemType = default;
+
+    [Tooltip("A prefab reference for the model of the item")]
+    [SerializeField]
+    private GameObject _prefab = default;
+
+
+    public string Name => _name;
+    public Sprite PreviewImage => _previewImage;
+    public string Description => _description;
+    public int price;
+    public ItemTypeSO ItemType => _itemType;
+    public GameObject Prefab => _prefab;
+
+}
