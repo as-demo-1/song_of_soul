fa105f25a9a9df63392644f54cd2b197032ac018
driver <1471551028@qq.com>
2021-12-05 23:46:01 +0800

    creat player by GameManager

--- Silksong/Assets/Scripts/SceneManage/GameObjectTeleporter.cs
+++ Silksong/Assets/Scripts/SceneManage/GameObjectTeleporter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 /// <summary>
 /// 负责游戏物体在场景内的传送 单例
 /// </summary>
@@ -32,6 +33,7 @@ public class GameObjectTeleporter : MonoBehaviour
     public Vector3 playerRebornPoint;//玩家最新的重生点
     public bool Transitioning;
 
+    public CinemachineVirtualCamera virtualCamera;
     void Awake()
     {
         if (Instance != this)
@@ -50,19 +52,24 @@ public class GameObjectTeleporter : MonoBehaviour
         //Instance.playerInput.transform.localScale = new Vector3(1, 0, 0);
         Teleport(Instance.playerInput.gameObject,Instance.playerRebornPoint);
     }
-    public static void playerEnterScene(SceneEntrance.EntranceTag entranceTag)//在玩家进入新场景时调用该方法
+    public  void playerEnterScene(SceneEntrance.EntranceTag entranceTag)//在玩家进入新场景时调用该方法
     {
         SceneEntrance entrance = SceneEntrance.GetDestination(entranceTag);
         if(entrance==null)//该场景没有入口 不是有玩家的游戏场景 
         {
             return;
         }
-        if (Instance.playerInput == null)
-            Instance.playerInput = FindObjectOfType<PlayerInput>();//
+        if (playerInput == null)
+            playerInput = FindObjectOfType<PlayerInput>();//
 
-        //Instance.playerInput.transform.localScale = new Vector3();角色朝向 暂未考虑
-        Instance.playerRebornPoint = entrance.transform.position;
-        Teleport(Instance.playerInput.gameObject, entrance.transform.position);
+        //playerInput.transform.localScale = new Vector3();角色朝向 暂未考虑
+        playerRebornPoint = entrance.transform.position;
+
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        if(virtualCamera)
+        virtualCamera.Follow = playerInput.transform;
+
+        Teleport(playerInput.gameObject, entrance.transform.position);
     }
     public static void Teleport(GameObject transitioningGameObject, Vector3 destinationPosition)
     {
