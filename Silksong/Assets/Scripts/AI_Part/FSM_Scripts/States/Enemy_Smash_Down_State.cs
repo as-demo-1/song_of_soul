b547f70f8efbc0d334900e83017319881ce1da26
driver <1471551028@qq.com>
2021-12-16 21:05:42 +0800

    add jump monster

--- Silksong/Assets/Scripts/AI_Part/FSM_Scripts/States/Enemy_Smash_Down_State.cs
+++ Silksong/Assets/Scripts/AI_Part/FSM_Scripts/States/Enemy_Smash_Down_State.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Enemy_Smash_Down_State : EnemyFSMBaseState
 {
     public float jumpHight;
+    public float downGravity;
     public string jumpAnimation;
     public string smashDownAnimation;
     public bool isMoveWithCurve;
@@ -20,12 +21,16 @@ public class Enemy_Smash_Down_State : EnemyFSMBaseState
         endPos = new Vector2(enemyFSM.player.transform.position.x, beginPos.y);
         hightest = beginPos.y + jumpHight;
         if (jumpAnimation != string.Empty)
-            enemyFSM.animator.Play(jumpAnimation);
+            enemyFSM.animator.Play(jumpAnimation,0);
     }
 
     public override void Act_State(EnemyFSMManager enemyFSM)
     {
         base.Act_State(enemyFSM);
+    }
+    public override void FixAct_State(EnemyFSMManager enemyFSM)
+    {
+        base.FixAct_State(enemyFSM);
         fsmManager = enemyFSM;
         stateInfo = enemyFSM.animator.GetCurrentAnimatorStateInfo(0);
         if (stateInfo.IsName(jumpAnimation))
@@ -33,29 +38,41 @@ public class Enemy_Smash_Down_State : EnemyFSMBaseState
             float t_tem = stateInfo.normalizedTime;
             if (t_tem < 0.99)
             {
-               endPos = fsmManager.player.transform.position;
+                if(t_tem > 0.2 && fsmManager.GetComponent<Collider2D>().IsTouchingLayers(1 << LayerMask.NameToLayer("Ground")))
+                {
+                    jumpOver();
+                }
+                //endPos = fsmManager.player.transform.position;
                 if (isMoveWithCurve)
                 {
                     Vector3 tem = new Vector2(Mathf.Lerp(beginPos.x, endPos.x, t_tem), Mathf.Lerp(beginPos.y, hightest, jumpCurve.Evaluate(t_tem)));
-                    fsmManager.transform.Translate(tem - fsmManager.transform.position);
+                    // fsmManager.transform.Translate(tem - fsmManager.transform.position);
+                    fsmManager.rigidbody2d.MovePosition(tem);
                 }
-                else 
+                else
                 {
                     Vector3 tem = new Vector2(Mathf.Lerp(beginPos.x, endPos.x, t_tem), Mathf.Lerp(beginPos.y, hightest, t_tem));
-                    fsmManager.transform.Translate(tem - fsmManager.transform.position);
+                    //fsmManager.transform.Translate(tem - fsmManager.transform.position);
+                    fsmManager.rigidbody2d.MovePosition(tem);
                 }
             }
-            else
+            
+            else  
             {
-                fsmManager.rigidbody2d.gravityScale = 20f;
-                if (smashDownAnimation != string.Empty)
-                    fsmManager.animator.Play(smashDownAnimation);
+                jumpOver();
             }
         }
         else if (stateInfo.IsName(smashDownAnimation))
         {
 
         }
+    }
+
+    private void jumpOver()
+    {
+        fsmManager.rigidbody2d.gravityScale = downGravity;
+        if (smashDownAnimation != string.Empty)
+            fsmManager.animator.Play(smashDownAnimation);
     }
     public override void ExitState(EnemyFSMManager enemyFSM)
     {
