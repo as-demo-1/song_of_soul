51d42dd6dd462e8193dc74075f947b4fc1015827
jiangzuojiben <603193489@qq.com>
2022-02-07 15:48:45 +0800

    增加水下八方向移动
    并修改了使角色能在水面上跳跃

--- Silksong/Assets/Scripts/Player/PlayerController/PlayerController.cs
+++ Silksong/Assets/Scripts/Player/PlayerController/PlayerController.cs
@@ -31,10 +31,11 @@ public class PlayerController : MonoBehaviour
 
     public PlayerAnimatorStatesControl PlayerAnimatorStatesControl { get; private set; }
 
-    public CharacterMoveControl PlayerHorizontalMoveControl { get; } 
+    public CharacterMoveControl PlayerHorizontalMoveControl { get; }
         = new CharacterMoveControl(1f, 5f, 8f, 8f, 10f);
-    
+
     public bool IsGrounded { get; set; }
+    public bool IsUnderWater { get; set; }
     public int CurrentAirExtraJumpCountLeft { get; private set; }
 
     public PlayerInfo playerInfo;
@@ -47,7 +48,7 @@ public class PlayerController : MonoBehaviour
     public Animator PlayerAnimator { get; private set; }
     //[SerializeField, HideInInspector]
     public Rigidbody2D RB { get; private set; }
-
+    public Transform m_Transform { get; set; }
     [SerializeField] private LayerMask groundLayerMask;
     //[SerializeField] private LayerMask ropeLayerMask; 注释理由：可能不再需要攀爬功能
 
@@ -79,8 +80,8 @@ public class PlayerController : MonoBehaviour
         else
             throw new UnityException("There cannot be more than one PlayerController script.  The instances are " + Instance.name + " and " + name + ".");
         DontDestroyOnLoad(this.gameObject);
-        if(_backpack)
-        _backpack.LoadSave();
+        if (_backpack)
+            _backpack.LoadSave();
     }
 
     private void OnEnable()
@@ -103,11 +104,34 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Colide with Item");
             _itemToAdd = other.gameObject;
         }
+        if (other.gameObject.CompareTag("UnderWater"))
+        {
+            float smooth = 0.1f;
+            //RB.velocity = Vector2.Lerp(RB.velocity, new Vector2(RB.velocity.x, 0), Time.time * smooth);
+            RB.gravityScale = playerInfo.gravity/2;
+            IsUnderWater = true;
+            //TODO: 禁用能力
+        }
+        if (other.gameObject.CompareTag("Water"))
+        {
+            //TODO: 禁用能力
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         _itemToAdd = null;
+        if (other.gameObject.CompareTag("UnderWater"))
+        {
+            RB.velocity += new Vector2(0, 5);   //在出水时给一个很大的出水速度
+            RB.gravityScale = playerInfo.gravity;
+            IsUnderWater = false;
+            //TODO: 恢复能力
+        }
+        if (other.gameObject.CompareTag("Water"))
+        {
+            //TODO: 禁用能力
+        }
     }
 
     public void CheckAddItem()
@@ -121,12 +145,13 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-    
+
 
     void Start()
     {
         // _saveSystem.TestSaveGuid(_guid);
         RB = GetComponent<Rigidbody2D>();
+        m_Transform = GetComponent<Transform>();
         //RB.sharedMaterial = new PhysicsMaterial2D() { bounciness = 0, friction = 0, name = "NoFrictionNorBounciness" };
         m_BodyCapsuleCollider = GetComponent<CapsuleCollider2D>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
@@ -137,6 +162,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (IsUnderWater) SwimUnderWater();
         CheckIsGroundedAndResetAirJumpCount();
         PlayerAnimatorStatesControl.ParamsUpdate();
     }
@@ -156,29 +182,29 @@ public class PlayerController : MonoBehaviour
         Interact();
     }
 
-   /* public void VerticalMove()
-    {
-        // check is on rope
-        if (IsRope())
-        {
-            // PInput.Vertical.Value onchange start climbing
-            if (PlayerInput.Instance.vertical.Value != 0)
-            {
-                OnClimb();
-            }
+    /* public void VerticalMove()
+     {
+         // check is on rope
+         if (IsRope())
+         {
+             // PInput.Vertical.Value onchange start climbing
+             if (PlayerInput.Instance.vertical.Value != 0)
+             {
+                 OnClimb();
+             }
 
-            // if jump unClimb
-            if (PlayerInput.Instance.jump.Down)
-            {
-                UnClimb();
-            }
-        }
-        // unClimb
-        else
-        {
-            UnClimb();
-        }
-    }*/
+             // if jump unClimb
+             if (PlayerInput.Instance.jump.Down)
+             {
+                 UnClimb();
+             }
+         }
+         // unClimb
+         else
+         {
+             UnClimb();
+         }
+     }*/
     public void CheckJump()
     {
         PlayerInput.Instance.jump.SetValidToFalse();
@@ -257,35 +283,81 @@ public class PlayerController : MonoBehaviour
         return IsBlock(ropeLayerMask);
     }*/
 
-   /* private void OnClimb()注释理由：可能不再需要攀爬功能
-    {
-        // velocity is rb current force
-        RB.velocity = Vector3.zero;
+    /* private void OnClimb()注释理由：可能不再需要攀爬功能
+     {
+         // velocity is rb current force
+         RB.velocity = Vector3.zero;
 
-        // if isClimb rb.pos is PInput.Vertical.Value
-        if (playerInfo.isClimb)
+         // if isClimb rb.pos is PInput.Vertical.Value
+         if (playerInfo.isClimb)
+         {
+             Vector2 pos = transform.position;
+             pos.y += playerInfo.climbSpeed * PlayerInput.Instance.vertical.Value * Time.deltaTime;
+             RB.MovePosition(pos);
+         }
+         // togging isClimb and gravityScale is 0
+         else
+         {
+             RB.gravityScale = 0;
+             playerInfo.isClimb = true;
+         }
+     }*/
+
+    /* private void UnClimb()
+     {
+         // togging isClimb and recovery gravityScale
+         if (playerInfo.isClimb)
+         {
+             RB.gravityScale = playerInfo.gravity;
+             playerInfo.isClimb = false;
+         }
+     }*/
+    public void SwimUnderWater()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
-            Vector2 pos = transform.position;
-            pos.y += playerInfo.climbSpeed * PlayerInput.Instance.vertical.Value * Time.deltaTime;
-            RB.MovePosition(pos);
+            m_Transform.localRotation = Quaternion.Euler(0, 0, 45);
+            RB.velocity = new Vector2(-1, 1).normalized * playerInfo.speed;
         }
-        // togging isClimb and gravityScale is 0
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            m_Transform.localRotation = Quaternion.Euler(0, 0, -45);
+            RB.velocity = new Vector3(1, 1).normalized * playerInfo.speed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            m_Transform.localRotation = Quaternion.Euler(0, 0, 135);
+            RB.velocity = new Vector2(-1, -1).normalized * playerInfo.speed;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            m_Transform.localRotation = Quaternion.Euler(0, 0, -135);
+            RB.velocity = new Vector2(1, -1).normalized * playerInfo.speed;
+        }
         else
         {
-            RB.gravityScale = 0;
-            playerInfo.isClimb = true;
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 0);
+                RB.velocity = new Vector2(0, 1) * playerInfo.speed;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 180);
+                RB.velocity = new Vector2(0, -1) * playerInfo.speed;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 90);
+                RB.velocity = new Vector2(-1, 0) * playerInfo.speed;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, -90);
+                RB.velocity = new Vector2(1, 0) * playerInfo.speed;
+            }
         }
-    }*/
-
-   /* private void UnClimb()
-    {
-        // togging isClimb and recovery gravityScale
-        if (playerInfo.isClimb)
-        {
-            RB.gravityScale = playerInfo.gravity;
-            playerInfo.isClimb = false;
-        }
-    }*/
+    }
 
     public void CheckFlipPlayer(float setAccelerationNormalizedTime)
     {
