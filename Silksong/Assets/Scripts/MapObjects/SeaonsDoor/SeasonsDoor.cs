9142678a84860890e0fddd2e9c2d3bcd1f292b9d
moonstoneG <1228070756@qq.com>
2022-02-27 19:23:03 +0800

    Update SeasonsDoor.cs

--- Silksong/Assets/Scripts/MapObjects/SeaonsDoor/SeasonsDoor.cs
+++ Silksong/Assets/Scripts/MapObjects/SeaonsDoor/SeasonsDoor.cs
@@ -8,7 +8,7 @@ public class SeasonsDoor : MonoBehaviour
 
     [SerializeField]private string NextMapName;
 
-    public void LoadNextMap()
+    public void LoadNextMap() //暂时方案
     {
         SceneManager.LoadScene(NextMapName);
     }
