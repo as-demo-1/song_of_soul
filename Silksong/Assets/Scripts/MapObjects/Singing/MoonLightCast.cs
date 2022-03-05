d15d88edbd37caf115e64775c8ddc9cd37af610d
xiaoming <1255330733@qq.com>
2022-03-05 14:43:45 +0800

    歌声及相关组件

--- Silksong/Assets/Scripts/MapObjects/Singing/MoonLightCast.cs
+++ Silksong/Assets/Scripts/MapObjects/Singing/MoonLightCast.cs
@@ -7,32 +7,34 @@ public class MoonLightCast : MonoBehaviour
     public LineRenderer line;
     public float g;
     public LayerMask Ground;
+    public Vector2 direct;
+    public Vector2 realDirect;
     GameObject reflectorLight;
     GameObject reflector;
     RaycastHit2D[] hit2Ds;
     PlayerController playerController;
-    Vector3 direct;
     // Start is called before the first frame update
     void Start()
     {
+        realDirect = (Vector2)transform.position + direct;
         hit2Ds = new RaycastHit2D[10];
-        line=GetComponent<LineRenderer>();
+        line = GetComponent<LineRenderer>();
         Physics2D.queriesStartInColliders = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
     void FixedUpdate()
     {
-        Radial(transform.position,transform.GetChild(0).position);
+        Radial(transform.position, realDirect);
         MoonLightEffect();
     }
     public Vector3 Radial(Vector2 shootPos, Vector2 target)
     {
-        line.SetPosition(0,transform.position);
+        line.SetPosition(0, transform.position);
         Vector3 direct = target - shootPos;
         RaycastHit2D hit = Physics2D.Raycast(shootPos, direct, 100, Ground);
         if (hit.collider != null)
@@ -47,17 +49,16 @@ public class MoonLightCast : MonoBehaviour
             return pos1;
         }
     }
-    public void RotateLight(GameObject light,GameObject reflector,Vector2 pos)
+    public void RotateLight(GameObject light, GameObject reflector, Vector2 pos)
     {
-        //可以把reflector.transform.right当作法线
-        light.transform.rotation = light.transform.rotation;
-        float z0 = reflector.transform.rotation.eulerAngles.z > 180 ? reflector.transform.rotation.eulerAngles.z - 180 : reflector.transform.rotation.eulerAngles.z;
-        float ZRotate = 2 * (90 - (transform.rotation.eulerAngles.z - z0));
-        light.transform.RotateAround(pos, new Vector3(0, 0, 1), ZRotate);
+        Vector2 dir = Vector2.Reflect(direct, reflector.transform.right);
+        light.GetComponent<MoonLightCast>().direct = dir;
+        light.transform.position = pos;
+        light.GetComponent<MoonLightCast>().realDirect = pos + dir;
     }
     public void MoonLightEffect()
     {
-        Physics2D.RaycastNonAlloc(transform.position, transform.GetChild(0).transform.position-transform.position, hit2Ds, 100);
+        Physics2D.RaycastNonAlloc(transform.position, direct, hit2Ds, 100);
         foreach (var hit in hit2Ds)
         {
             if (!hit) continue;
@@ -69,17 +70,14 @@ public class MoonLightCast : MonoBehaviour
                     playerController.RB.gravityScale = g;
                 }
             }
-            else if (hit.collider.GetComponent<Reflector>()&&hit.collider.gameObject!=reflector)
+            else if (hit.collider.GetComponent<Reflector>() && hit.collider.gameObject != reflector)
             {
                 if (reflectorLight == null)
                 {
                     reflectorLight = Instantiate(gameObject);
                     reflectorLight.GetComponent<MoonLightCast>().reflector = hit.collider.gameObject;
-                    reflectorLight.transform.position = hit.point;
-                    reflectorLight.transform.localScale = new Vector3(reflectorLight.transform.localScale.x, -reflectorLight.transform.localScale.y, reflectorLight.transform.localScale.z);                    
-                    RotateLight(reflectorLight, hit.collider.gameObject, hit.point);
                 }
-                
+                RotateLight(reflectorLight, hit.collider.gameObject, hit.point);
             }
         }
         //if (playerController && playerController.gravityLock == false)
