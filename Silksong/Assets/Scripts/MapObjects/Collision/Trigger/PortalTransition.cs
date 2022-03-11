ccd9c41765000a47e2aeb5a7c407c17afe3b96bf
WAi <why18520259443@gmail.com>
2022-03-11 14:41:18 +0800

    add PortalTrasition
    传送后场景人物重复

---
+++ Silksong/Assets/Scripts/MapObjects/Collision/Trigger/PortalTransition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTransition : SceneTransitionPoint
+{
+    private bool canTrans;
+    public GameObject TransitionPoint;
+    private GameObject player;
+
+    void Update()
+    {
+        if (canTrans)
+        {
+            enterEvent();       //切换到指定场景
+            //player移动到指定位置
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = true;
+            player = other.gameObject;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = false;
+        }
+    }
+}
