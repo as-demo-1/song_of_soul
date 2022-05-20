e3f4ab3752c267da0a23cb3a99e914bcaf2fc72e
Iambananayeah <56341827+Iambananayeah@users.noreply.github.com>
2022-05-20 10:47:50 +0800

    add SMBwwiseEvent

--- Silksong/Assets/Scripts/Player/SMBEvent/Event/SMBEvent.cs
+++ Silksong/Assets/Scripts/Player/SMBEvent/Event/SMBEvent.cs
@@ -34,60 +34,12 @@ public abstract class SMBEventTimeStamp : SMBEvent
 {
     [Range(0, 1)]
     public float timeNormalized;
-    /*public override bool Next(float previousTimeNormalized)
-    {
-        return timeNormalized <= previousTimeNormalized;
-    }*/
-    //public override bool IsActive(float previousTimeNormalized, float currentTimeNormalized)
+
     public override bool IsActive(float currentTimeNormalized)
     {
         //return timeNormalized > previousTimeNormalized && timeNormalized <= currentTimeNormalized;
         return this.timeNormalized <= currentTimeNormalized;
     }
-  /* public override bool NotEndYet(float currentTimeNormalized)
-    {
-        return this.timeNormalized <= currentTimeNormalized;
-    }*/
+
     public override float FieldToCompare => timeNormalized;
 }
-
-//public abstract class SMBEventTimeRange : SMBEvent
-//{
-//    [Range(0, 1)]
-//    public float startTimeNormalized;
-//    [Range(0, 1)]
-//    public float endTimeNormalized;
-
-//    public override bool Next(float previousTimeNormalized)
-//    {
-//        return this.endTimeNormalized <= previousTimeNormalized;
-//    }
-
-//    public override bool IsActive(float previousTimeNormalized, float currentTimeNormalized)
-//    {
-//        return !(this.startTimeNormalized > currentTimeNormalized || this.endTimeNormalized <= previousTimeNormalized);
-//    }
-
-//    public override bool NotEndYet(float currentTimeNormalized)
-//    {
-//        return this.startTimeNormalized <= currentTimeNormalized;
-//    }
-
-//    public override int CompareTo(SMBEvent other)
-//    {
-//        if (this.FieldToCompare != other.FieldToCompare)
-//            return this.FieldToCompare - other.FieldToCompare >= 0 ? 1 : -1;
-//        else
-//        {
-//            if (other is SMBEventTimeRange)
-//            {
-//                return this.endTimeNormalized - (other as SMBEventTimeRange).endTimeNormalized >= 0 ? 1 : -1;
-//            }
-//            else
-//            {
-//                return this.endTimeNormalized - other.FieldToCompare >= 0 ? 1 : -1;
-//            }
-//        }
-//    }
-//    public override float FieldToCompare => startTimeNormalized;
-//}
