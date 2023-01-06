ab62548badfc3fd857db834e5e8f599f2ae0ea5a
moonstoneG <76591064+moonstoneG@users.noreply.github.com>
2023-01-06 20:10:10 +0800

    Boss

---
+++ Silksong/Assets/Scripts/AI_Part/BigBird/CheckHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    public class CheckHealth : Conditional
+    {
+        private Boss_BigBird BirdController;
+        private GameObject Player;
+        private float Health = 50;
+        public override void OnStart()
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+           // BirdController = Player.GetComponentInChildren<Boss_BigBird>();
+            
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (Health > 100)
+                return TaskStatus.Success;
+            else
+            {
+                return TaskStatus.Failure;
+                
+            }
+        }
+    }
+}
---
+++ Silksong/Assets/Scripts/AI_Part/BigBird/ClawAttack.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    public class ClawAttack : Action
+    {
+        public float Speed = 5;
+        private Animator animator;
+        public Rigidbody2D body;
+        private Transform Player;
+
+        public override void OnAwake()
+        {
+            Player = GameObject.FindGameObjectWithTag("Player").transform;
+            body = gameObject.GetComponentInChildren<Rigidbody2D>();
+            animator = gameObject.GetComponentInChildren<Animator>();
+            
+        }
+
+        public override void OnBehaviorRestart()
+        {
+          
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+
+            Vector2 target = new Vector2(Player.position.x, Player.position.y - 1.0f);
+            Vector2 res = Vector2.MoveTowards(body.position, target, Speed * Time.fixedDeltaTime);
+            body.MovePosition(res);
+            animator.SetTrigger("Attack_Claw");
+            Debug.Log(body.position.y);
+            if (body.position.y == -4.246644f)
+            {
+
+                Vector2 targetup = new Vector2(body.position.x, -2f);
+                Vector2 resup = Vector2.MoveTowards(body.position, targetup, Speed * Time.fixedDeltaTime);
+                body.MovePosition(resup);
+                Debug.Log("1");
+             
+                return TaskStatus.Inactive;
+              
+
+
+            }
+
+            return TaskStatus.Running;
+
+        }
+    }
+}
