0f0ad81aac43e0c5e9cb36a6a9e7c9a7b48785ab
yxshi610 <yxshi610@gmail.com>
2023-04-24 22:59:39 +0800

    增加二阶段动画、加音符特效

--- Silksong/Assets/Scripts/AI_Part/Final Boss/Piano.cs
+++ Silksong/Assets/Scripts/AI_Part/Final Boss/Piano.cs
@@ -22,24 +22,40 @@ public class Piano : MonoBehaviour
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         float height = leftup.transform.localPosition.y - rightdown.transform.localPosition.y;
         float width = rightdown.transform.localPosition.x - leftup.transform.localPosition.x;
-        float cameraX = transform.position.x;
-        float cameraY = transform.position.y;
+        //float height = 2 * Camera.main.orthographicSize;
+        //float width = height * Camera.main.aspect;
+        //float cameraX = transform.position.x;
+        //float cameraY = transform.position.y;
+        float centerX = leftup.transform.localPosition.x + width / 2.0f;
+        float centerY = rightdown.transform.localPosition.y + height / 2.0f;
 
         float cellWidth = width / numCols;
         float cellHeight = height;
 
+        GameObject bg = Instantiate(key, transform);
+        bg.name = "bg";
+        bg.transform.position = transform.position;
+        bg.transform.parent = transform;
+        SpriteRenderer sr = bg.AddComponent<SpriteRenderer>();
+        sr.sprite = Sprite.Create(Texture2D.whiteTexture, new Rect(0f, 0f, 1f, 1f), new Vector2(0.5f, 0.5f));
+        sr.color = Color.white;
+        sr.color += new Color(0, 0, 0, -0.5f);
+        sr.sortingOrder = 1;
+        sr.drawMode = SpriteDrawMode.Sliced;
+        sr.size = new Vector3(width * 2, height, 1f);
+
         for (int col = 0; col < numCols; col++)
         {
-            // 计算单元格的中心坐标 1.28 magic number why?
-            float x = cameraX - width / 2 * 1.28f + cellWidth * (col + 0.5f) * 1.28f;
-            float y = cameraY;
+            //float x = cameraX - width / 2 * 1.28f + cellWidth * (col + 0.5f) * 1.28f;
+            float x = centerX - width / 2.0f  + cellWidth * (col + 0.5f) ;
+            float y = centerY;
             Vector3 cellPosition = new Vector3(x, y, 0f);
 
             // 实例化 Prefab
             GameObject cellObject = Instantiate(key, transform);
             // 修改实例化后的物体名称
             cellObject.name = "Key " + col;
-            cellObject.transform.position = cellPosition;
+            cellObject.transform.localPosition = cellPosition;
             cellObject.transform.parent = transform;
 
             // 添加一个SpriteRenderer组件
@@ -48,20 +64,19 @@ public class Piano : MonoBehaviour
             if (chartList[currentChart][col] == '0')
             {
                 spriteRenderer.color = Color.white;
-                spriteRenderer.color += new Color(0, 0, 0, -1);
+                spriteRenderer.color += new Color(0, 0, 0, -0.5f);
+                spriteRenderer.sortingOrder = 1;
             }
             else
             {
                 spriteRenderer.color = Color.black;
                 spriteRenderer.color += new Color(0, 0, 0, -0.5f);
-                spriteRenderer.sortingOrder = 1;
+                spriteRenderer.sortingOrder = 2;
             }
             spriteRenderer.drawMode = SpriteDrawMode.Sliced;
 
             // 设置单元格的大小
             spriteRenderer.size = new Vector3(cellWidth, cellHeight, 1f);
-
-            
         }
     }
 
@@ -72,13 +87,13 @@ public class Piano : MonoBehaviour
     public void Attack()
     {
         //this.gameObject.SetActive(true);
-        if (this.transform.childCount <= 2)
+        if (this.transform.childCount <= 3)
         {
             Generate();
         }
         for (int col = 0; col < numCols; ++col)
         {
-            GameObject cellObject = transform.GetChild(col+2).gameObject;
+            GameObject cellObject = transform.GetChild(col+3).gameObject;
             SpriteRenderer spriteRenderer = cellObject.GetComponent<SpriteRenderer>();
             BoxCollider2D boxCollider = cellObject.GetComponent<BoxCollider2D>();
             spriteRenderer.color += new Color(0, 0, 0, 0.5f);
---
+++ Silksong/Assets/Scripts/AI_Part/Final Boss/WordSystem/WordPart.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordPart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 impulse;
+    private Rigidbody2D rb;
+    private float slow = 2000.0f;
+    private GameObject damager;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 velocity = rb.velocity;
+        Vector3 direction = velocity.normalized;
+        if (Mathf.Abs(velocity.y) < 0.1f)
+        {
+            rb.AddForce(direction * (-1) * slow * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
+    }
+
+    private void Awake()
+    {
+        SetDamager();
+        Impulse();
+    }
+
+    void SetDamager()
+    {
+        damager = GameObject.Find("Boss").transform.GetChild(0).gameObject;
+        GameObject d = Instantiate(damager, this.gameObject.transform);
+        Destroy(d.GetComponent<BoxCollider2D>());
+        UnityEditorInternal.ComponentUtility.CopyComponent(this.gameObject.GetComponent<PolygonCollider2D>());
+        UnityEditorInternal.ComponentUtility.PasteComponentAsNew(d);
+        d.GetComponent<PolygonCollider2D>().isTrigger = true;
+    }
+
+
+    void Impulse()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.AddForce(impulse, ForceMode2D.Impulse); ;
+    }
+}
