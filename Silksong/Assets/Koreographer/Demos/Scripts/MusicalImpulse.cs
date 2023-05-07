4ecc0b0f87a738c3bf905505598e6c0a7d374731
yxshi610 <yxshi610@gmail.com>
2023-05-07 12:45:20 +0800

    加入Koreo音乐打点插件

---
+++ Silksong/Assets/Koreographer/Demos/Scripts/MusicalImpulse.cs
@@ -0,0 +1,47 @@
+﻿//----------------------------------------------
+//            	   Koreographer                 
+//    Copyright © 2014-2020 Sonic Bloom, LLC    
+//----------------------------------------------
+
+using UnityEngine;
+
+namespace SonicBloom.Koreo.Demos
+{
+	[RequireComponent(typeof(Rigidbody))]
+	[AddComponentMenu("Koreographer/Demos/Musical Impulse")]
+	public class MusicalImpulse : MonoBehaviour
+	{
+		[EventID]
+		public string eventID;
+		public float jumpSpeed = 3f;
+
+		Rigidbody rigidbodyCom;
+
+		void Start()
+		{
+			// Register for Koreography Events.  This sets up the callback.
+			Koreographer.Instance.RegisterForEvents(eventID, AddImpulse);
+
+			rigidbodyCom = GetComponent<Rigidbody>();
+		}
+
+		void OnDestroy()
+		{
+			// Sometimes the Koreographer Instance gets cleaned up before hand.
+			//  No need to worry in that case.
+			if (Koreographer.Instance != null)
+			{
+				Koreographer.Instance.UnregisterForAllEvents(this);
+			}
+		}
+		
+		void AddImpulse(KoreographyEvent evt)
+		{
+			// Add impulse by overriding the Vertical component of the Velocity.
+			Vector3 vel = rigidbodyCom.velocity;
+			vel.y = jumpSpeed;
+
+			rigidbodyCom.velocity = vel;
+		}
+	}
+}
