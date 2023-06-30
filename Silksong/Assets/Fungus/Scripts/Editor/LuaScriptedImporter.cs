2c7300ca3e04c49f806dd41360d8359d1e1a85ae
蓑笠翁 <49121173+vighlive@users.noreply.github.com>
2023-06-30 20:40:43 +0800

    111

---
+++ Silksong/Assets/Fungus/Scripts/Editor/LuaScriptedImporter.cs
@@ -0,0 +1,22 @@
+﻿// This code is part of the Fungus library (https://github.com/snozbot/fungus)
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
+using UnityEngine;
+
+using System.IO;
+
+namespace Fungus.EditorUtils
+{
+#if UNITY_2018_4_OR_NEWER
+    [UnityEditor.AssetImporters.ScriptedImporter(1, "lua")]
+	public class LuaScriptedImporter : UnityEditor.AssetImporters.ScriptedImporter
+	{
+	    public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext ctx)
+	    {
+	        TextAsset lua = new TextAsset(File.ReadAllText(ctx.assetPath));
+	        ctx.AddObjectToAsset("main", lua);
+	        ctx.SetMainObject(lua);
+	    }
+	}
+#endif
+}
