d772cb6f3cf730e1381c9ea3e1087c44a6c01fca
ddsurvivor <63942441+ddsurvivor@users.noreply.github.com>
2023-07-13 13:55:05 +0800

    添加demo场景

--- Silksong/Assets/Scripts/UI/Tutorial/TutorialTrigger.cs
+++ Silksong/Assets/Scripts/UI/Tutorial/TutorialTrigger.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class TutorialTrigger : MonoBehaviour
 {
     [SerializeField] private string tagToDetect = "Player";
-    [SerializeField] private string title;
+    [SerializeField] private GameObject panel;
 
-	[SerializeField] private TutorialPanelSO TutorialPanelSO;
+    private bool hasTriggered;
+    
+	//[SerializeField] private TutorialPanelSO TutorialPanelSO;
+
+	private void Awake()
+	{
+		panel.SetActive(false);
+	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log("Trigger");
+		//Debug.Log("Trigger");
 		if (other.CompareTag(tagToDetect))
 		{
-			TutorialPanelSO.RasieEvent(title);
-			Debug.Log("Player Trigger");
+			panel.SetActive(true);
+			//panel.transform.DOShakeScale(0.5f, 0.2f);
+			hasTriggered = true;
+			//TutorialPanelSO.RasieEvent(title);
+			PlayerInput.Instance.ReleaseControls();// Ω˚”√ÕÊº“ ‰»Î
+			Debug.Log("Player Trigger Tutorial");
 		}
 
+	}
+	/*private void OnTriggerExit2D(Collider2D other)
+	{
+		//Debug.Log("Trigger");
+		if (other.CompareTag(tagToDetect))
+		{
+			panel.SetActive(false);
+			//TutorialPanelSO.RasieEvent(title);
+			Debug.Log("Player leave Tutorial");
+		}
+
+	}*/
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.J) && hasTriggered)
+		{
+			panel.SetActive(false);
+			PlayerInput.Instance.GainControls();
+			//this.enabled = false;
+		}
 	}
 }
